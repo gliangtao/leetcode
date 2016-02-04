cd78c4408a9e0a56b15e6da6430eea5b0cf9aa1f
Liangtao Gao <gltdev@sina.com>
2016-02-03 16:27:07 -0800

    Add Locked
    Signed-off-by: Liangtao Gao <gltdev@sina.com>

---
+++ locked/closest-binary-search-tree-value/Solution.java
@@ -0,0 +1,22 @@
+/**
+Given a non-empty binary search tree and a target value, find the value in the BST that is closest
+to the target.
+
+Note:
+Given target value is a floating point.
+You are guaranteed to have only one unique value in the BST that is closest to the target.
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int closestValue(TreeNode root, double target) {
+
+    }
+}
---
+++ locked/one-edit-distance/Solution.java
@@ -0,0 +1,8 @@
+/**
+Given two strings S and T, determine if they are both one edit distance apart.
+*/
+public class Solution {
+    public boolean isOneEditDistance(String s, String t) {
+
+    }
+}
